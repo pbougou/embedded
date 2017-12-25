42badc742e8f0ddcd4f0aef3eb08c4ee0fed9c3d
pbougou <pbougou@gmail.com>
2017-12-25 19:22:36 +0200

    update - not done yet

--- askhsh2/2.2/ask2.2-2017.s
+++ askhsh2/2.2/ask2.2-2017.s
@@ -6,7 +6,8 @@
 
 
 main:
-	push {ip, lr} 
+	push {ip, lr}
+
 	ldr r0, =string
 	bl printf
 	ldr r0, =input
@@ -16,78 +17,86 @@ main:
 	bl strlen  
 	cmp r0, #33
 	movgt r0, #33 @ r0 has length of input string 
+	mov r7, r0
 
 	ldr r10, =str
 	ldrb r3, [r10] @ load first char of string in r10
 
 	cmp r0, #1
-	bne main_loop
+	bne loop
 	cmp r3, #81
 	beq exit
 	cmp r3, #113
-	beq exit @ if 'q' or 'Q' and length 1, exit
+	beq exit 	@ if 'q' or 'Q' and length 1, exit
+
+
 @ 2.2 starts
-
-
-
-main_loop:
-	cmp r3, #47
+@ ldr r4, =frequency @r4 points at start of frequency array
+loop:
+	ldr r4, =frequency @r4 points at start of frequency array
+	cmp r3, #32
 	ble done
-	cmp r3, #57
-	ble number
-	cmp r3, #64
-	ble done
-	cmp r3, #90
-	ble upper
-	cmp r3, #96
-	ble done
-	cmp r3, #122
-	ble lower
-	bal done
-
-number:
-	cmp r3, #52
-	addle r3, r3, #5
-	subgt r3, r3, #5
-	bal done
-
-upper:
-	add r3, r3, #32
-	bal done
-	
-lower:
-	sub r3, #32
+	cmp r3, #126
+	bgt done
+	sub r3, r3, #33
+	add r4, r4, r3
+	ldrb r5, [r4]
+	add r5,r5,#1
+	strb r5, [r4]
 	
 done:
-	strb r3, [r10]
 	ldrb r3, [r10, #1]!
 	subs r0, r0, #1
-	bne main_loop
-	
-	
+	bne loop
 
 
-	ldr r0, =output_str
-	ldr r1, =str
-	bl printf
-	ldr r0, =output_int
-@	ldr r1, =len
-@	bl printf
+ldr r4, =frequency @r4 points at start of frequency array
+ldr r10, =str @ r4 still points at start of frequency array 
+ldrb r1, [r10]
+@ r7 has string length
+print_loop:
+	ldr r4, =frequency @r4 points at start of frequency array
+	ldr r0, =output_msg @ output message format
+	
+	sub r3, r1, #33 @ indexing in frequency array
+	add r4, r4, r3
+	ldr r2, [r4]	 
+	cmp r2, #0	@ if frequency[r3] == 0, don't print.
+	blne printf
+	mov r2, #0
+	strb r2, [r4]
+
+@ next byte(char) in str 
+	ldrb r1, [r10, #1]!
+	subs r7, r7, #1
+	bne print_loop
 	pop {ip, pc} 
 
 exit:
 	ldr r0, =exit_str
 	bl printf
-	add r0,r0,r0
 	pop {ip, pc} 
 
 .data
-	string: .ascii "Input a string up to 32 chars long: \0"
-	input: .ascii "%s\0"
-	str: .ascii "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+	string: 
+		.ascii "Input a string up to 32 chars long: \0"
+	input: 
+		.ascii "%[^\n]s\0"
+	str: 
+		.ascii "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+
 	len = . - str
-	output_str: .ascii "%s\n\0"
-	output_int: .ascii "%d\n\0"
-	exit_str: .ascii "Exiting.\n\0"
-    frequency .word 94
+
+	output_str: 
+		.ascii "%s\n\0"
+	output_msg: 
+		.ascii "%c -> %d\n\0"
+	output_int: 
+		.ascii "%d\n"
+	exit_str: 
+		.ascii "Exiting.\n\0"
+@ byte addressing logic
+	.balign 4
+    	frequency: 
+		.skip 94
 .end
