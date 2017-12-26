ecd286668e37b8e21344c1da06ad0cb3b3f15352
pbougou <pbougou@gmail.com>
2017-12-26 22:15:52 +0200

    with open and write (with bugs also)

--- askhsh2/2.2/ask2.2-2017.s
+++ askhsh2/2.2/ask2.2-2017.s
@@ -3,11 +3,19 @@
 .extern printf
 .extern scanf
 .extern strlen
-
+.extern snprintf
 
 main:
 	push {ip, lr}
 
+	@ Open file
+	ldr r0, =filename
+	movw r1, #65 @ #1092 @ O_RDWR | O_CREAT | O_APPEND
+	mov r7, #5
+	swi 0
+	@ r0 has file descriptor
+	push { r0 }
+	
 	ldr r0, =string
 	bl printf
 	ldr r0, =input
@@ -17,7 +25,8 @@ main:
 	bl strlen  
 	cmp r0, #33
 	movgt r0, #33 @ r0 has length of input string 
-	mov r7, r0
+	mov r7, r0    @ store it in r7
+	push {r7}
 
 	ldr r10, =str
 	ldrb r3, [r10] @ load first char of string in r10
@@ -31,7 +40,7 @@ main:
 	
 
 @ 2.2 starts
-@ ldr r4, =frequency @r4 points at start of frequency array
+ldr r4, =frequency @r4 points at start of frequency array
 loop:
 	ldr r4, =frequency @r4 points at start of frequency array
 	cmp r3, #32
@@ -50,13 +59,15 @@ done:
 	bne loop
 
 
+
+/*
 ldr r4, =frequency @r4 points at start of frequency array
 ldr r10, =str @ r4 still points at start of frequency array 
 ldrb r1, [r10]
 mov r14, #0
 @ r7 has string length
 print_loop:
-	ldr r4, =frequency @r4 points at start of frequency array
+	@ldr r4, =frequency @r4 points at start of frequency array
 	ldr r0, =output_msg @ output message format
 	
 	sub r3, r1, #33 @ indexing in frequency array
@@ -72,7 +83,54 @@ print_loop:
 	ldrb r1, [r10, #1]!
 	subs r7, r7, #1
 	bne print_loop
-	
+*/
+pop { r7 } 
+pop { r0 }
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Don' t use r0... It has fd for write@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ldr r8, =frequency @ r8 points at start of frequency array
+ldr r10, =str      @ r10 points at start of str
+ldrb r3, [r10]     @ character for pretty printing
+mov r14, #0	   @ always zero
+ 
+write_loop:
+@ r3 has the ascii 
+@ r4 has the frequency for this ascii
+	ldr r8, =frequency 	 @ r8 points at start of frequency array
+	sub r5, r3, #33          @ indexing in frequency array
+	add r8, r8, r5		 @ address of desired character in r8
+	ldrb r4, [r8]	 	 @ frequency of current character
+
+	cmp r4, #0		 @ if character at r3(ascii) has frequency 
+				 @ equal to #0, don't print.
+	beq next
+				 @ else ...
+	strb r14, [r8]	 	 @ make zero the frequency, 
+				 @ so as not to print again
+	ldr r0, =buffer    
+	ldr r1, =BUFLEN
+	ldr r2, =format_msg
+@ r3 has the ascii for character to be printed	
+	push { r0, r7, r8, r10, r14 }
+	bl snprintf		 @ buffer created. Now write it to file
+	pop { r0, r7, r8, r10, r14 }
+
+@ write system call (#4):
+	mov r0, #1
+	ldr r1, =buffer
+	ldr r2, =BUFLEN
+	push { r0, r7 }
+	mov r7, #4 		 @ write system call
+	swi 0
+	pop { r0, r7 }
+
+@ next byte(char) in str
+next:
+	ldrb r3, [r10, #1]!
+	subs r7, r7, #1
+	bne write_loop
+
 	pop {ip, pc}
 
 exit:
@@ -81,22 +139,30 @@ exit:
 	pop {ip, pc}
 
 .data
+	filename:
+		.ascii "outfile\0"
 	string: 
 		.ascii "Input a string up to 32 chars long: \0"
 	input: 
 		.ascii "%[^\n]s\0"
 	str: 
 		.ascii "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
-	len = . - str
+	buffer: 
+		.ascii "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+	BUFLEN = . - buffer
 	output_str: 
 		.ascii "%s\n\0"
 	output_msg: 
 		.ascii "%c -> %ld\n\0"
+	format_msg: 
+		.ascii "%c -> %d\n\0"
 	output_int: 
 		.ascii "%d\n\0"
 	exit_str: 
 		.ascii "Exiting.\n\0"
 	frequency:
 		.ascii "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+	trans:
+		.ascii " -> "
 		
 .end
