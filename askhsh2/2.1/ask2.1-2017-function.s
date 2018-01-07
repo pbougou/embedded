a777c0e3caa362c48e0b5228a2a2053587a0b94f
pavlos.kapoutsis <pavloskapoutsis@gmail.com>
2018-01-07 23:12:10 +0200

    --

--- askhsh2/2.1/ask2.1-2017-function.s
+++ askhsh2/2.1/ask2.1-2017-function.s
@@ -7,33 +7,7 @@
 .extern scanf
 .extern strlen
 .extern getchar
-/*
-main:
-	push {ip, lr} 
-again:
-	ldr r0, =string
-	bl printf
 
-	ldr r0, =input
-	ldr r1, =str
-	bl scanf
-	bl getchar
-
-	ldr r0, =str
-	bl strlen  
-	cmp r0, #33
-	movgt r0, #33
-
-	ldr r10, =str
-	ldrb r3, [r10]
-
-	cmp r0, #1
-	bne main_loop
-	cmp r3, #81
-	beq exit
-	cmp r3, #113
-	beq exit
-*/
 transform:
 	push {ip, lr} 
 transform_loop:
@@ -71,32 +45,4 @@ done:
 	bne transform_loop
 	
 	pop {ip, pc} 
-/*	
 
-
-	ldr r0, =output_str
-	ldr r1, =str
-	bl printf
-	ldr r0, =output_int
-@	ldr r1, =len
-@	bl printf
-
-	bal again
-	pop {ip, pc} 
-
-exit:
-	ldr r0, =exit_str
-	bl printf
-	add r0,r0,r0
-	pop {ip, pc} 
-
-.data
-	string: .ascii "Input a string up to 32 chars long: \0"
-	input: .ascii "%[^\n]s\0"
-	str: .ascii "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
-	len = . - str
-	output_str: .ascii "%s\n\0"
-	output_int: .ascii "%d\n\0"
-	exit_str: .ascii "Exiting.\n\0"
-.end
-*/
